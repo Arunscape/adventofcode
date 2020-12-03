57e4d8ea2d48854ca6cef4b4c0df9aa04349c82a
Arunscape <arun@woosaree.xyz>
2020-12-03 00:08:51 -0700

    just had a showerthought: can use line.chars().cycle() instead of doing the modulo math myself ok.. now I go to bed for real

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -3,7 +3,11 @@ use std::io::{self, BufRead};
 fn main() {
     let stdin = io::stdin();
 
-    let ans = stdin.lock().lines().flatten();
+    let ans = stdin
+        .lock()
+        .lines()
+        .flatten()
+        .map(|line| line.chars().cycle());
 
     println!("{}", ans);
 }
