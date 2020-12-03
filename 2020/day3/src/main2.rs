c09638f3990f98b924e9a2b06e2d18069de73ff1
Arunscape <arun@woosaree.xyz>
2020-12-03 16:58:40 -0700

    why isn't it working?!

---
+++ 2020/day3/src/main2.rs
@@ -0,0 +1,67 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+
+    let input: Vec<_> = stdin.lock().lines().flatten().enumerate().collect();
+
+    let slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)];
+
+    let ans: usize = slopes
+        .iter()
+        .map(|&slope| ski(input.iter(), slope))
+        .map(|val| {
+            println!("{}", val);
+            val
+        })
+        .product();
+
+    println!("{}", ans)
+}
+
+fn ski<'a>(it: impl Iterator<Item = &'a (usize, String)>, (rise, run): (usize, usize)) -> usize {
+    it.step_by(rise)
+        .filter(|(lineno, line)| line.chars().cycle().nth(lineno * run).unwrap() == '#')
+        .count()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! ski_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+    let input: Vec<_> = "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+        .lines()
+        .map(|s| s.to_string())
+        .enumerate()
+        .collect();
+
+                let (expected, slope) = $value;
+                assert_eq!(expected, ski(input.iter(), slope));
+            }
+        )*
+    }
+    }
+
+    ski_tests!(
+    ski0: (2, (1,1)),
+    ski1: (7, (1,3)),
+    ski2: (3, (1,5)),
+    ski3: (4, (1,7)),
+    ski4: (2, (2,1)),
+    );
+}
