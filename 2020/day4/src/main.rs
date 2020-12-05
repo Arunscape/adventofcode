82660158cfdf19c3ca4baf3603a09d0e302f0500
Arunscape <arun@woosaree.xyz>
2020-12-04 18:44:37 -0700

    first gold star for day 4

--- 2020/day4/src/main.rs
+++ 2020/day4/src/main.rs
@@ -4,9 +4,26 @@ fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    let p: Vec<_> = buffer.split("\n\n").collect();
+    let passports: Vec<_> = buffer.split("\n\n").collect();
 
-    println!("{:?}", p);
+    let ans = passports
+        .iter()
+        .filter(|&passport| {
+            let p: Vec<_> = passport.split_whitespace().collect();
+
+            if p.iter().count() == 8 {
+                return true;
+            }
+
+            if p.iter().count() == 7 {
+                return p.iter().all(|field| !field[..3].eq("cid"));
+            }
+
+            false
+        })
+        .count();
+
+    println!("{}", ans);
 
     Ok(())
 }
