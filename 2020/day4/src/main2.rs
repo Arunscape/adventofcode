076b793194e8716c8faa7951fdeed7a0ffc92e0d
Arunscape <arun@woosaree.xyz>
2020-12-05 00:54:12 -0700

    I need to sleep and then debug this with a fresh mind

---
+++ 2020/day4/src/main2.rs
@@ -0,0 +1,86 @@
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
+    let passports: Vec<_> = buffer.split("\n\n").collect();
+
+    let ans = passports
+        .iter()
+        .filter(|&passport| {
+            let mut count = 0;
+
+            for field in passport.split_whitespace() {
+                println!("{}", &field[..3]);
+                match &field[..3] {
+                    "byr" => {
+                        println!("should be between 1920-2002: {}", &field[4..]);
+                        let year: usize = field[4..].parse().unwrap();
+                        if (1920..=2002).contains(&year) {
+                            count += 1;
+                        }
+                    }
+                    "iyr" => {
+                        println!("should be between 2010-2020: {}", &field[4..]);
+                        let year: usize = field[4..].parse().unwrap();
+                        if (2010..=2020).contains(&year) {
+                            count += 1;
+                        }
+                    }
+                    "eyr" => {
+                        println!("should be between 2010-2020: {}", &field[4..]);
+                        let year: usize = field[4..].parse().unwrap();
+                        if (2010..=2030).contains(&year) {
+                            count += 1;
+                        }
+                    }
+                    "hgt" => {
+                        println!("should be in or cm: {}", &field[field.len() - 2..]);
+                        if match &field[field.len() - 2..] {
+                            "cm" => {
+                                let height = field[4..7].parse();
+                                println!("height: {:?}", height);
+                                height.is_ok() && (150..=193).contains(&height.unwrap())
+                            }
+                            "in" => {
+                                let height = field[4..6].parse();
+                                println!("height: {:?}", height);
+                                height.is_ok() && (59..=76).contains(&height.unwrap())
+                            }
+                            _ => false,
+                        } {
+                            count += 1;
+                        }
+                    }
+                    "hcl" => {
+                        if field[4..].chars().all(char::is_alphanumeric) {
+                            count += 1;
+                        }
+                    }
+                    "ecl" => {
+                        if ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+                            .iter()
+                            .any(|&c| c.eq(&field[4..]))
+                        {
+                            count += 1;
+                        }
+                    }
+                    "pid" => {
+                        if field[4..].chars().all(char::is_numeric) {
+                            count += 1;
+                        }
+                    }
+                    "cid" => {}
+                    _ => panic!("invalid field"),
+                }
+            }
+            println!("count: {}", count);
+            count == 7
+        })
+        .count();
+
+    println!("{}", ans);
+
+    Ok(())
+}
