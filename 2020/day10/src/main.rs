e51551d76ffcb11947a6cbb689434b688a8124fa
Arunscape <arun@woosaree.xyz>
2020-12-10 17:18:40 -0700

    part 1 done

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,3 +1,26 @@
+use itertools::Itertools;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input: Vec<usize> = std::io::stdin()
+        .lock()
+        .lines()
+        .flatten()
+        .flat_map(|s| s.parse())
+        .collect();
+
+    input.sort_unstable();
+
+    let mut one_jolt = 1;
+    let mut three_jolt = 1;
+
+    for (x, y) in input.iter().tuple_windows() {
+        match y - x {
+            1 => one_jolt += 1,
+            3 => three_jolt += 1,
+            _ => {}
+        }
+    }
+
+    println!("1 jolt: {}\n3 jolt: {}", one_jolt, three_jolt);
 }
