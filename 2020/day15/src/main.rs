e91a4772c602908d76eabc6221f17fb92b822569
Arunscape <arun@woosaree.xyz>
2020-12-23 00:08:18 -0700

    day15

--- 2020/day15/src/main.rs
+++ 2020/day15/src/main.rs
@@ -11,12 +11,12 @@ struct Game {
 impl Game {
     pub fn new(starting_nums: &[usize]) -> Self {
         let mut hashmap = HashMap::new();
-        let index = starting_nums.len() - 1;
-        let last_said = starting_nums[index];
+        let index = starting_nums.len();
+        let last_said = starting_nums[index - 1];
 
         // assuming no repeats in starting_nums
         for (idx, &num) in starting_nums.iter().enumerate() {
-            let v = vec![idx];
+            let v = vec![idx + 1];
             hashmap.insert(num, v);
         }
 
@@ -38,17 +38,13 @@ impl Game {
 impl Iterator for Game {
     type Item = usize;
     fn next(&mut self) -> Option<usize> {
-        dbg!(&self);
-        println!("Turn {}", self.index);
         if self.first_time_last_has_been_spoken() {
             self.index += 1;
-            let v = self.hashmap.get_mut(&0).unwrap();
-            v.push(self.index);
-            println!(
-                "{} was first spoken at index {}, saying 0",
-                self.last_said,
-                v[v.len() - 2]
-            );
+            if let Some(v) = self.hashmap.get_mut(&0) {
+                v.push(self.index);
+            } else {
+                self.hashmap.insert(0, vec![self.index]);
+            }
             self.last_said = 0;
             return Some(0);
         }
@@ -59,16 +55,13 @@ impl Iterator for Game {
         let last_said_index = previously_spoken_indexes[previously_spoken_indexes.len() - 2];
 
         self.last_said = self.index - last_said_index;
-        println!("{} had been spoken before", self.last_said);
-        println!("current index: {}", self.index);
-        println!("last spoken index: {}", last_said_index);
         self.index += 1;
         if let Some(v) = self.hashmap.get_mut(&self.last_said) {
             v.push(self.index);
         } else {
             self.hashmap.insert(self.last_said, vec![self.index]);
         }
-        Some(dbg!(self.last_said))
+        Some(self.last_said)
     }
 }
 
@@ -83,9 +76,9 @@ fn main() -> io::Result<()> {
         .flat_map(str::parse::<usize>)
         .collect();
 
-    let mut game = Game::new(dbg!(&starting_nums));
+    let mut game = Game::new(&starting_nums);
 
-    let ans = game.nth(10 - starting_nums.len()).unwrap();
+    let ans = game.nth(30000000 - starting_nums.len() - 1).unwrap();
 
     println!("{}", ans);
     Ok(())
