e610a7e3e804ae1ecb9bf87d2b94839f8d4e883b
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-12-10 15:23:22 -0700

    day 9

--- 2022/day9/src/main.rs
+++ 2022/day9/src/main.rs
@@ -1,34 +1,38 @@
 use std::collections::HashSet;
 use std::io::{self, BufRead};
 
+type Point = (isize, isize);
 #[derive(Debug)]
 struct Rope {
-    head: (isize, isize),
-    tail: (isize, isize),
-    tail_visited: HashSet<(isize, isize)>,
+    rope: Vec<Point>,
+    tail_visited: HashSet<Point>,
 }
 
 impl Rope {
-    pub fn new() -> Self {
-        let head = (0, 0);
-        let tail = (0, 0);
-//        let mut tail_visited = HashSet::new();
+    pub fn new(length: usize) -> Self {
+        assert!(2 <= length);
+        let rope = vec![(0, 0); length];
         let tail_visited = HashSet::new();
-//        tail_visited.insert(tail);
 
         Self {
-            head,
-            tail,
+            rope,
             tail_visited,
         }
     }
 
+    pub fn tail(&self) -> Point {
+        *self.rope.last().unwrap()
+    }
+
+    pub fn head(&self) -> Point {
+        self.rope[0]
+    }
+
     pub fn move_head(&mut self, direction: Direction, n: usize) -> (isize, isize) {
         if n < 1 {
-            return self.head;
+            return self.head();
         }
-        let (ref mut x, ref mut y) = self.head;
-        let (ref mut i, ref mut j) = self.tail;
+        let (ref mut x, ref mut y) = self.rope[0];
         match direction {
             Direction::Up => *y += 1,
             Direction::Down => *y -= 1,
@@ -36,16 +40,33 @@ impl Rope {
             Direction::Left => *x -= 1,
         };
 
-        let (dy, dx) = (*y - *j, *x - *i);
-
-    
-        // todo rethink this
-        self.tail_visited.insert(self.tail);
-
+        let tail = self.update_tail(1);
+        self.tail_visited.insert(tail);
+        
         self.move_head(direction, n - 1);
 
-        self.head
+        self.head()
     }
+
+    pub fn update_tail(&mut self, index: usize) -> Point {
+        let (x, y) = self.rope[index-1];
+        let (ref mut i, ref mut j) = self.rope[index];
+        let (dy, dx) = (y - *j, x - *i);
+
+        if 1 < dy.abs() || 1 < dx.abs() {
+            *i += dx.signum();
+            *j += dy.signum();
+        }
+
+        if index <  self.rope.len() - 1 {
+            self.update_tail(index + 1);
+        }
+        
+        //println!("head: {:?} tail: {:?}", self.head(), self.tail());
+
+        self.tail()
+    }
+
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -77,15 +98,20 @@ fn main() {
         })
         .collect();
 
-    let mut rope = Rope::new();
+    let mut rope = Rope::new(2);
 
-    //dbg!(&rope);
     for &(d, n) in input.iter() {
         rope.move_head(d, n);
-        //dbg!(&rope);
     }
 
     let p1 = rope.tail_visited.len();
-
     println!("part 1: {p1}");
+
+
+    let mut rope = Rope::new(10);
+    for &(d, n) in input.iter() {
+        rope.move_head(d, n);
+    }
+    let p2 = rope.tail_visited.len();
+    println!("part 2: {p2}");
 }
