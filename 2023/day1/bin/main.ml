0dfb5b31cbbacaec79ea2f579b62823619d66840
Arunscape <8227297+Arunscape@users.noreply.github.com>
2023-12-05 00:21:53 -0700

    well here's part 1

--- 2023/day1/bin/main.ml
+++ 2023/day1/bin/main.ml
@@ -1,45 +1,77 @@
-let rec sum l = match l with
-  | [] -> 0
-  | head :: tail -> head + (sum tail)
-;;
+open Base
 
-let char2int c = int_of_char c - int_of_char '0';;
-let str2charlist s = s |> String.to_seq |> List.of_seq;; (* what I don't get this *)
 
-let rec first_digit s: int option = 
-  match s with
-  | [] -> None
-  | head :: tail -> match head with
-    | '0' .. '9' -> Some (char2int head)
-    | _ -> first_digit tail
-;;
-
-let rec read_input l =
-  match read_line () with
-  | exception End_of_file -> List.rev l
-  | line -> read_input (line :: l)
-;;
-
-let () = 
-  let input = read_input [] in
-  let output = input |> List.map str2charlist |> List.map first_digit in
-  output |> List.iter (fun x -> match x with | Some x -> print_int x | None -> ())
+let sum l = 
+  let rec inner t l = match l with
+  | [] -> t
+  | head :: tail -> inner (t + head) tail
+  in inner 0 l
 ;;
 
 
-(*
-let rec first_digit (s: char Seq.t): int option =
-  match s () with
-  | Seq.Nil -> None
-  | Seq.Cons (head, tail) -> match head with
-    | '0' .. '9' -> Some (char2int head)
-    | _ -> first_digit tail
+let read_input () : string list =
+  Stdio.In_channel.input_lines In_channel.stdin
+
+
+let is_digit c = match c with
+  | '0' .. '9' -> true
+  | _ -> false
+;;
+
+let first_digit s = match String.lfindi s ~f:(fun _ c -> is_digit c) with 
+  | None -> None
+  | Some (i) -> Some(String.get s i)
+;;
+
+let last_digit s = match String.rfindi s ~f:(fun _ c -> is_digit c) with
+  | None -> None
+  | Some (i) -> Some(String.get s i)
 ;;
 
 
 let () = 
   let input = read_input () in
-  let o = input.map first_digit in
-  o.iter (fun x -> match x with | Some x -> print_int x | None -> ())
+  let part_one = input
+  |> List.map ~f:(fun s -> (first_digit s, last_digit s)) 
+  |> List.map ~f:(fun x -> match x with
+    | (Some a, Some b) -> Char.to_string a ^ Char.to_string b
+    | _ -> "None") 
+  |> List.map ~f:Int.of_string
+  |> sum
+  |> Int.to_string in
+
+  Stdio.print_endline part_one;
+;;
+
+  
+(*
+let char2int c = int_of_char c - int_of_char '0';;
+*)
+
+(* gonna use iter and containers instead
+let str2charlist s = s |> String.to_seq |> List.of_seq;;
+*)
+
+(*
+let rec first_digit (s:char list): char option = 
+  match s with
+  | [] -> None
+  | head :: tail -> match head with
+    | '0' .. '9' -> Some (head)
+    | _ -> first_digit tail
+;;
+
+let last_digit s =
+  first_digit (List.rev s)
+;;
+*)
+
+(*
+let read_input () =
+  let rec inner l = 
+  match Stdio.In_channel.input_line with
+  | exception End_of_file -> List.rev l
+  | line -> inner (line :: l)
+  in inner [] 
 ;;
 *)
