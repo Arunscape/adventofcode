51cb4f2eb4be1e0bf30ca570d9baadd9b3db477f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2025-12-02 18:53:26 -0700

    day 2 part 2

--- 2025/day2/bin/main.ml
+++ 2025/day2/bin/main.ml
@@ -1,23 +1,19 @@
 open Base
 open Aoc_utils
 
-let is_even n = n % 2 = 0
-
 (* let is_invalid_id n =
   let s = Int.to_string n in
   let len = String.length s in
   is_even len && String.equal (String.sub s ~pos:0 ~len:(len / 2)) (String.sub s ~pos:(len / 2) ~len:(len / 2)) *)
 
-let is_invalid_id n =
-  let num_digits = Int.of_float (Float.log10 (Float.of_int n)) + 1 in
-  if is_even num_digits then 
+let repeated_twice n =
+  let num_digits = num_digits n in
+  if num_digits |> is_even then
     let div = 10 ** (num_digits / 2) in
-    n/div = n % div
-else false
+    n / div = n % div
+  else false
 
-let invalid_ids_in_range min max = 
-  List.range min (max + 1)
-  |> List.filter ~f:is_invalid_id
+let invalid_ids_in_range min max f = range_inclusive min max |> List.filter ~f
 
 let parse_input input =
   String.concat input ~sep:""
@@ -25,19 +21,35 @@ let parse_input input =
   |> String.split ~on:','
   |> List.filter ~f:(fun s -> not (String.is_empty s))
   |> List.map ~f:(fun s ->
-    match String.split s ~on:'-' with
-    | [min_s; max_s] -> (Int.of_string min_s, Int.of_string max_s)
-    | _ -> failwith ("Invalid range format: " ^ s)
-  )
+      match String.split s ~on:'-' with
+      | [ min_s; max_s ] -> (Int.of_string min_s, Int.of_string max_s)
+      | _ -> failwith ("Invalid range format: " ^ s))
 
-let solve_1 ranges =
+let solve ranges f =
   List.fold ranges ~init:0 ~f:(fun acc (min, max) ->
-    let invalid_ids = invalid_ids_in_range min max in
-    acc + (List.sum (module Int) invalid_ids ~f:Fn.id)
-  )
+      let invalid_ids = invalid_ids_in_range min max f in
+      acc + List.sum (module Int) invalid_ids ~f:Fn.id)
+
+let get_divisors n =
+  range_inclusive 1 (n / 2) |> List.filter ~f:(fun i -> n |> is_multiple_of i)
+
+let repeated_at_least_once n =
+  let num_digits = num_digits n in
+  let divisors = get_divisors num_digits in
+  List.exists divisors ~f:(fun len_of_repeat ->
+      let n_repeats = num_digits / len_of_repeat in
+      let multiplier =
+        List.range 0 n_repeats
+        |> List.fold ~init:0 ~f:(fun acc i ->
+            acc + Int.pow 10 (i * len_of_repeat))
+      in
+      n |> is_multiple_of multiplier)
+
+let solve_1 ranges = solve ranges repeated_twice
+let solve_2 ranges = solve ranges repeated_at_least_once
 
 let () =
   let input = read_input () in
   let ranges = parse_input input in
-  let total_invalid_sum = solve_1 ranges in
-  Stdio.printf "Part 1: %d\n" total_invalid_sum
+  Stdio.printf "Part 1: %d\n" (solve_1 ranges);
+  Stdio.printf "Part 2: %d\n" (solve_2 ranges)
--- lib/ocaml/aoc_utils.ml
+++ lib/ocaml/aoc_utils.ml
@@ -6,7 +6,9 @@ let sum l =
   in
   inner 0 l
 
-let read_input () : string list = Stdio.In_channel.input_lines Stdio.In_channel.stdin
+let read_input () : string list =
+  Stdio.In_channel.input_lines Stdio.In_channel.stdin
+
 let is_digit c = match c with '0' .. '9' -> true | _ -> false
 
 let first_digit s =
@@ -18,3 +20,9 @@ let last_digit s =
   match String.rfindi s ~f:(fun _ c -> is_digit c) with
   | None -> None
   | Some i -> Some (String.get s i)
+
+let is_multiple_of n m = m % n = 0
+let is_even n = n |> is_multiple_of 2
+let is_odd n = is_even n |> not
+let num_digits n = Int.of_float (Float.log10 (Float.of_int n)) + 1
+let range_inclusive min max = List.range min max ~stop:`inclusive
